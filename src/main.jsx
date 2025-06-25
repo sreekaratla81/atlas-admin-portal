bad28c7a93c45cbad068cbeb4a9ad3230e72be8c
sreekaratla81 <150553417+sreekaratla81@users.noreply.github.com>
2025-06-26 00:53:58 +0530

    Comment out Auth0 setup for temporary open access

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 import './style.css';
 
-const domain = "atlashomestays.us.auth0.com";
-const clientId = "d70OGzWag10f4viX8DI1SxOXAj6aDsvX";
+// const domain = "atlashomestays.us.auth0.com";
+// const clientId = "d70OGzWag10f4viX8DI1SxOXAj6aDsvX";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
+    {/* <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{ redirect_uri: window.location.origin }}
-    >
+    > */}
       <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
